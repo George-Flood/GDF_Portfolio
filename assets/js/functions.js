ebab697f7fc09a9a2540f9218d384aaa80f4533d
George Flood <georgeflood44@gmail.com>
2016-05-09 21:22:17 +0100

    Header
    Coded the header

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-  console.log('Hello World');
+
+  $('.infobtn').click(function() {
+    $(this).toggleClass('act');
+    $('.header').toggleClass('openheader');
+
+    setTimeout(function() {
+      $('.info-lockup').toggleClass('openholder');
+    }, 250);
+
+    $('body').toggleClass('noscroll');
+  });
+
+
 });
