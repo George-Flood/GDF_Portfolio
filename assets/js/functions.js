9f69565774dfdfa71e77db3b7ed9d64ca6a4324a
George Flood <georgeflood44@gmail.com>
2016-05-10 22:29:46 +0100

    Main Page
    Main Page Coded!

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
 
+  workLoad();
+
+  $(window).load(function() {
+		$('article').addClass('is-open');
+	});
+
+
+
   $('.infobtn').click(function() {
     $(this).toggleClass('act');
     $('.header').toggleClass('openheader');
@@ -10,6 +18,48 @@ $( document ).ready(function() {
 
     $('body').toggleClass('noscroll');
   });
+
+
+
+  $('article').click(function() {
+    $('.work').addClass('workopen');
+    $('.thumbs').addClass('thumbsclose');
+  });
+
+  $('body').on("click", ".exit", function() {
+    $('.work').removeClass('workopen');
+    $('.thumbs').removeClass('thumbsclose');
+  });
+
+  $('article').on("click", function() {
+    $('html, body').scrollTop(0);
+  });
+
+
+  function workLoad() {
+
+    $.ajaxSetup({
+      cache: false
+    });
+
+
+
+    $('article').click(function() {
+
+      var $this = $(this),
+        spinner = '<div class="loader"></div>',
+        newFolder = $this.data('folder'),
+        newHTML = '.assets/work/' + newFolder + '.html';
+
+
+      //history.pushState({}, '', newFolder);
+      $('.work').html(spinner).load(newHTML);
+      //return false;
+
+    });
+
+  }
+
 
 
 });
