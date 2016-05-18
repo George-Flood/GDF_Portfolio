2fdeb2eb0349c91d567b78480b908ae8ca9d57fa
George Flood <georgeflood44@gmail.com>
2016-05-18 23:20:37 +0100

    History API

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 
 
 
-  $('.infobtn').click(function() {
+  $('.infobtn').click(function(e) {
     $(this).toggleClass('act');
     $('.header').toggleClass('openheader');
 
@@ -18,7 +18,21 @@ $( document ).ready(function() {
 
     $('html, body').toggleClass('noscroll');
     $('.logo').toggleClass('white');
+    e.stopPropagation();
   });
+
+  $('.header').click(function(e) {
+		$('.infobtn').removeClass('act');
+		$('.header').removeClass('openheader');
+
+		setTimeout(function() {
+			$('.info-lockup').removeClass('openholder');
+		}, 250);
+
+		$('html, body').removeClass('noscroll');
+    $('.logo').removeClass('white');
+    e.stopPropagation();
+	});
 
 
 
@@ -30,27 +44,41 @@ $( document ).ready(function() {
   $('body').on("click", ".exit", function() {
     $('.work').removeClass('workopen');
     $('.thumbs').removeClass('thumbclose');
+    history.pushState(null, '', '/');
   });
 
   $('article').on("click", function() {
     $('html, body').scrollTop(0);
   });
+
+
+  function workLoad() {
+
+    $.ajaxSetup({
+      cache: true
+    });
+
+    $('article').click(function() {
+
+      var $this = $(this),
+        spinner = '<div class="loader"></div>',
+        newFolder = $this.data('folder'),
+        newHTML = 'assets/work/' + newFolder + '.html';
+      $('.work').html(spinner).load(newHTML);
+
+      history.pushState(newFolder, '', newFolder);
+
+    });
+
+  }
+
+  window.addEventListener('popstate', function(e) {
+		var character = e.state;
+
+		if (character === null) {
+			$('.work').removeClass('workopen');
+			$('.thumbs').removeClass('thumbclose');
+		} else {
+		}
+	});
 });
-
-function workLoad() {
-
-  $.ajaxSetup({
-    cache: true
-  });
-
-  $('article').click(function() {
-
-    var $this = $(this),
-      spinner = '<div class="loader"></div>',
-      newFolder = $this.data('folder'),
-      newHTML = 'assets/work/' + newFolder + '.html';
-    $('.work').html(spinner).load(newHTML);
-
-  });
-
-}
